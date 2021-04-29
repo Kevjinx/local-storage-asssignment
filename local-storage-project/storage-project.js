0ce9630c97d36f8522cdf1ea1d348b835f89c726
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-04-29 16:40:36 -0400

    PROJECT - DONE

--- local-storage-project/storage-project.js
+++ local-storage-project/storage-project.js
@@ -4,24 +4,55 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const storedItems = document.getElementById('items')
     const item = storedItems.value
     const quantity = document.getElementById('quantity') 
-    localStorage.setItem(item, quantity.value)
+    localStorage.setItem(item, quantity.value) 
   })
   
-const showCart = () => {
- const list = document.getElementById('cartItems')
-   for(let key in localStorage) {
+  
+  const list = document.getElementById('cartItems')
+  const showCart = () => {
+   for(let i = 0; i < localStorage.length; i++){
+     let key = localStorage.key(i)
      const listItem = document.createElement('li')
      listItem.innerHTML = key + ': ' + localStorage.getItem(key)
+     const remove = document.createElement('button');
+     remove.innerHTML = 'Remove'
+     remove.setAttribute('class', 'removeButton')
+     remove.setAttribute('type', 'submit')
+     remove.id = key
+     listItem.appendChild(remove)
      list.appendChild(listItem)
+     console.log()
    }
   };
-
+showCart()
   // const storeItem = () => {
 
-  // };
-
-  // const removeItem = () => {
 
   // };
+  const removeItem = () => {
+    const button = document.querySelectorAll('.removeButton');
+    console.log(button)
+    button.forEach(function(ele){
+      ele.addEventListener('click', event => {
+        localStorage.removeItem(event.target.id)
+        location.reload()
+      })
+    })
+  }
+  /* list.addEventListener('click', event => {
+    console.log(event.target)
+    event.preventDefault()
+  }) */
+ 
 
+removeItem() 
 });
+
+
+
+
+/*  console.log(removeButton)
+  removeButton.forEach(function(element){
+    element.addEventListener('submit', event => {
+    })
+  }) */ 
