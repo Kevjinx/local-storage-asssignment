55ed01c781fad14bbf9db9acc94a920659408051
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-04-29 13:43:23 -0400

    Phase 1 - Done

--- local-storage-project/storage-project.js
+++ local-storage-project/storage-project.js
@@ -1,4 +1,11 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  const addToCart = document.getElementById('add-to-cart');
+  addToCart.addEventListener('click', event => {
+    const storedItems = document.getElementById('items')
+    const item = storedItems.value
+    const quantity = document.getElementById('quantity') 
+    localStorage.setItem(item, quantity.value)
+  })
 
   // const showCart = () => {
 
